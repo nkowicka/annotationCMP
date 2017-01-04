fb0f27174f588605feb3fa358051fedc12ffe409
nkowicka <kamila.nowicka@student.uw.edu.pl>
2017-01-05 00:17:33 +0100

    Create skrypt.sh
    tool annotationCMP
    Using:
    you must download all files from https://github.com/nkowicka/annotationCMP
    and use comand
    (in folder with all these files)
    ./skrypt -m [metod] -e [evelue] -f [file]
    where:
        -m | --metod  :choose databese cazy, merops or phi
        -e  | --evalue  :select evelue in format 1e-9 when you have 0,000000001
        -f   | --file        :select file which you have use to annotation

---
+++ skrypt.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+while :
+do
+   case "$1" in
+      -m | --metod)
+         metod="$2"
+         shift 2
+         ;;
+      -e | --evalue)
+         evalue="$2"
+         shift 2
+         ;;
+      -f | --file)
+         file="$*"
+         shift 2
+         ;;
+     # -*)
+     #    echo "Error"
+     #    exit 1
+     #    ;;
+      *) #no more options
+         break
+         ;;
+   esac
+done
+
+echo $file
+
+if [ $# = "0" ]; then
+   if [ "$metod" = "cazy" ]; then
+      ./cazy.sh $evalue $file /home/kamilan/cazy/auxenochlorella_protothecoides.fasta /home/kamilan/cazy/chlorella_variabilis.fasta /home/kamilan/cazy/helicosporidium_unip_proteins.fasta /home/kamilan/cazy/Saccharomyces_cerevisiae.fasta
+      echo "jestem"
+
+   elif [ "$metod" = "merops" ]; then
+      ./mer.sh $evalue $file /home/kamilan/cazy/auxenochlorella_protothecoides.fasta /home/kamilan/cazy/chlorella_variabilis.fasta /home/kamilan/cazy/helicosporidium_unip_proteins.fasta /home/kamilan/cazy/Saccharomyces_cerevisiae.fasta
+
+   elif [ "$metod" = "phi" ]; then
+      ./phi.sh $evalue $file /home/kamilan/cazy/auxenochlorella_protothecoides.fasta /home/kamilan/cazy/chlorella_variabilis.fasta /home/kamilan/cazy/helicosporidium_unip_proteins.fasta /home/kamilan/cazy/Saccharomyces_cerevisiae.fasta
+   else echo "Błędnie wprowadzona komenda. Schemat: ./skrypt -m nazwa_metody -e evalue -p pliki , gdzie nazwa_metody to możliwe jedno z: cazy, merops lub phi . Evalue powinno być w formacie np. 1e-9  . Natomiast pliki są ścieżkami dostępu plików FASTA na których ma zostać przeprowadzaona annotacja"
+   fi
+
+else
+   if [ "$metod" = "cazy" ]; then
+      ./cazy.sh $evalue $file
+      echo "jestem2"
+
+   elif [ "$metod" = "merops" ]; then
+      ./mer.sh $evalue $file 
+
+   elif [ "$metod" = "phi" ]; then
+      ./phi.sh $evalue $file 
+   else echo "Błędnie wprowadzona komenda. Schemat: ./skrypt -m nazwa_metody -e evalue -p pliki , gdzie nazwa_metody to możliwe jedno z: cazy, merops lub phi . Evalue powinno być w formacie np. 1e-9  . Natomiast pliki są ścieżkami dostępu plików FASTA na których ma zostać przeprowadzona annotacja"
+   fi
+fi
