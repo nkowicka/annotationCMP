0791221d85ef98836711a5bdbc704d183c7fe906
nkowicka <kamila.nowicka@student.uw.edu.pl>
2017-01-05 00:21:42 +0100

    Create phi.sh
    Script to use PHI database for USEARCH and create table of results

---
+++ phi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./usearch_phi.sh $*
+
+touch AX
+touch CH
+touch HS
+touch PW
+touch SC
+
+
+sed -e "s/#/|/g" ./phi/wyniki_$1/wyniki_phi_AX > AX
+sed -e "s/#/|/g" ./phi/wyniki_$1/wyniki_phi_CH > CH
+sed -e "s/#/|/g" ./phi/wyniki_$1/wyniki_phi_HS > HS
+sed -e "s/#/|/g" ./phi/wyniki_$1/wyniki_phi_PW > PW
+sed -e "s/#/|/g" ./phi/wyniki_$1/wyniki_phi_SC > SC
+
+python tabelkap.py
+
+#./query_phi.sh $*
